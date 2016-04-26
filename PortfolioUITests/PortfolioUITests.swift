53733a836317fe3ba88282516860644520017f5d
John Woolsey <john.w.woolsey@gmail.com>
2016-04-26 09:38:23 -0500

    Added Watch List versions of existing Portfolio UI tests.

--- PortfolioUITests/PortfolioUITests.swift
+++ PortfolioUITests/PortfolioUITests.swift
@@ -21,11 +21,6 @@ class PortfolioUITests: XCTestCase {
    
    var appWindow: XCUIElement { return app.windows.elementBoundByIndex(0) }
    var keyboardReturnButton: XCUIElement { return app.buttons["Return"] }
-   var portfolioTab: XCUIElement { return app.tabBars.buttons["Portfolio"] }
-   var watchListTab: XCUIElement { return app.tabBars.buttons["Watch List"] }
-   var portfolioAddButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Add"] }
-   var portfolioEditButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Edit"] }
-   var portfolioDoneButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Done"] }
    var editLabel: XCUIElement { return app.staticTexts["Edit Mode Enabled"] }
    var dismissRegion: XCUIElement { return XCUIApplication().otherElements["PopoverDismissRegion"] }
    var addPositionAlertView: XCUIElement { return app.alerts["Add Position"] }
@@ -37,6 +32,16 @@ class PortfolioUITests: XCTestCase {
    var confirmDeleteSheetDeleteButton: XCUIElement { return app.sheets["Confirm Delete"].buttons["Delete"] }
    var cellAAPL: XCUIElement { return cells.staticTexts["AAPL"] }
    var cellTSLA: XCUIElement { return cells.staticTexts["TSLA"] }
+   
+   var portfolioTab: XCUIElement { return app.tabBars.buttons["Portfolio"] }
+   var portfolioAddButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Add"] }
+   var portfolioEditButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Edit"] }
+   var portfolioDoneButton: XCUIElement { return app.navigationBars["Portfolio"].buttons["Done"] }
+   
+   var watchListTab: XCUIElement { return app.tabBars.buttons["Watch List"] }
+   var watchListAddButton: XCUIElement { return app.navigationBars["Watch List"].buttons["Add"] }
+   var watchListEditButton: XCUIElement { return app.navigationBars["Watch List"].buttons["Edit"] }
+   var watchListDoneButton: XCUIElement { return app.navigationBars["Watch List"].buttons["Done"] }
    
    var cells: XCUIElementQuery { return app.collectionViews.cells }  // TODO: Need distinct Portfolio and Watch List versions?
    
@@ -74,6 +79,7 @@ class PortfolioUITests: XCTestCase {
     Requests then cancels Portfolio position addition UI tests.
     */
    func testPortfolioAddWithCancel() {
+      XCTAssertEqual(navigationBarTitle, "Portfolio")
       XCTAssertEqual(cells.count, 0, "Incorrect number of Portfolio cells.")
       portfolioAddButton.tap()
       XCTAssertTrue(addPositionAlertView.exists, "Add position alert view does not exist.")
@@ -85,9 +91,26 @@ class PortfolioUITests: XCTestCase {
    
    
    /**
+    Requests then cancels Watch List position addition UI tests.
+    */
+   func testWatchListAddWithCancel() {
+      watchListTab.tap()
+      XCTAssertEqual(navigationBarTitle, "Watch List")
+      XCTAssertEqual(cells.count, 0, "Incorrect number of Watch List cells.")
+      watchListAddButton.tap()
+      XCTAssertTrue(addPositionAlertView.exists, "Add position alert view does not exist.")
+      XCTAssertTrue(CGRectContainsRect(appWindow.frame, addPositionAlertView.frame), "Add position alert view is not visible.")
+      addPositionCancelButton.tap()
+      XCTAssertFalse(addPositionAlertView.exists, "Add position alert view still exists.")
+      XCTAssertEqual(cells.count, 0, "Incorrect number of Watch List cells.")
+   }
+   
+   
+   /**
     Portfolio position editing UI tests.
     */
    func testPortfolioPositionEditing() {
+      XCTAssertEqual(navigationBarTitle, "Portfolio")
       XCTAssertEqual(cells.count, 0, "Incorrect number of Portfolio cells.")
 
       // Add AAPL position
@@ -116,7 +139,7 @@ class PortfolioUITests: XCTestCase {
       // Delete AAPL position
       deletePositionAAPL()
       // Testing decrementing collection view cell count not working properly due to cell caching
-      // XCTAssertEqual(portfolioCells.count, 0, "Incorrect number of Portfolio cells.")
+      // XCTAssertEqual(cells.count, 0, "Incorrect number of Portfolio cells.")
       XCTAssertFalse(cellAAPL.hittable, "AAPL cell is still hittable.")
       XCTAssertTrue(cellTSLA.hittable, "TSLA cell is not hittable.")
       
@@ -124,6 +147,49 @@ class PortfolioUITests: XCTestCase {
       disablePortfolioEditing()
    }
    
+   
+   /**
+    Watch List position editing UI tests.
+    */
+   func testWatchListPositionEditing() {
+      watchListTab.tap()
+      XCTAssertEqual(navigationBarTitle, "Watch List")
+      XCTAssertEqual(cells.count, 0, "Incorrect number of Watch List cells.")
+      
+      // Add AAPL position
+      addWatchListPositionAAPL()
+      XCTAssertEqual(cells.count, 1, "Incorrect number of Watch List cells.")
+      
+      // Add TSLA position
+      addWatchListPositionTSLA()
+      XCTAssertEqual(cells.count, 2, "Incorrect number of Watch List cells.")
+      
+      // Enable position editing
+      enableWatchListEditing()
+      
+      // Request then cancel AAPL deletion
+      cellAAPL.doubleTap()
+      if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+         dismissRegion.tap()
+      } else {
+         confirmDeleteSheetCancelButton.tap()
+      }
+      XCTAssertEqual(cells.count, 2, "Incorrect number of Watch List cells.")
+      
+      // Move TSLA position to AAPL
+      swapPositions()
+      
+      // Delete AAPL position
+      deletePositionAAPL()
+      // Testing decrementing collection view cell count not working properly due to cell caching
+      // XCTAssertEqual(cells.count, 0, "Incorrect number of Watch List cells.")
+      XCTAssertFalse(cellAAPL.hittable, "AAPL cell is still hittable.")
+      XCTAssertTrue(cellTSLA.hittable, "TSLA cell is not hittable.")
+      
+      // Disable position editing
+      disableWatchListEditing()
+   }
+
    
    // MARK: - Helper Functions (includes assertions)
    
@@ -148,6 +214,24 @@ class PortfolioUITests: XCTestCase {
    
    
    /**
+    Adds AAPL position to Watch List.
+    
+    Keyboard Add button utilized.
+    */
+   func addWatchListPositionAAPL() {
+      watchListAddButton.tap()
+      XCTAssertTrue(addPositionAlertView.exists, "Add position alert view does not exist.")
+      XCTAssertTrue(CGRectContainsRect(appWindow.frame, addPositionAlertView.frame), "Add position alert view is not visible.")
+      addPositionSymbolTextField.typeText("aapl")
+      addPositionAddButton.tap()
+      expectationForPredicate(NSPredicate(format: "exists == 1"), evaluatedWithObject: cellAAPL, handler: nil)
+      waitForExpectationsWithTimeout(5, handler: nil)
+      XCTAssertFalse(addPositionAlertView.exists, "Add position alert view still exists.")
+      XCTAssertTrue(cellAAPL.hittable, "AAPL cell is not hittable.")
+   }
+
+   
+   /**
     Adds TSLA position to Portfolio.
     
     Text based returns utilized.
@@ -166,6 +250,23 @@ class PortfolioUITests: XCTestCase {
    
    
    /**
+    Adds TSLA position to Watch List.
+    
+    Text based return utilized.
+    */
+   func addWatchListPositionTSLA() {
+      watchListAddButton.tap()
+      XCTAssertTrue(addPositionAlertView.exists, "Add position alert view does not exist.")
+      XCTAssertTrue(CGRectContainsRect(appWindow.frame, addPositionAlertView.frame), "Add position alert view is not visible.")
+      addPositionSymbolTextField.typeText("tsla\r")
+      expectationForPredicate(NSPredicate(format: "exists == 1"), evaluatedWithObject: cellTSLA, handler: nil)
+      waitForExpectationsWithTimeout(5, handler: nil)
+      XCTAssertFalse(addPositionAlertView.exists, "Add position alert view still exists.")
+      XCTAssertTrue(cellTSLA.hittable, "TSLA cell is not hittable.")
+   }
+
+   
+   /**
     Enables Portfolio editing mode.
     */
    func enablePortfolioEditing() {
@@ -176,6 +277,16 @@ class PortfolioUITests: XCTestCase {
    
    
    /**
+    Enables Watch List editing mode
+    */
+   func enableWatchListEditing() {
+      watchListEditButton.tap()
+      XCTAssertTrue(editLabel.exists, "Edit mode view does not exist.")
+      XCTAssertTrue(CGRectContainsRect(appWindow.frame, editLabel.frame), "Edit mode view is not visible.")
+   }
+
+   
+   /**
     Disables Portfolio editing mode.
     */
    func disablePortfolioEditing() {
@@ -183,6 +294,15 @@ class PortfolioUITests: XCTestCase {
       XCTAssertFalse(editLabel.exists, "Edit mode view still exists.")
    }
    
+   
+   /**
+    Disables Watch List editing mode.
+    */
+   func disableWatchListEditing() {
+      watchListDoneButton.tap()
+      XCTAssertFalse(editLabel.exists, "Edit mode view still exists.")
+   }
+
    
    /**
     Deletes AAPL position.
